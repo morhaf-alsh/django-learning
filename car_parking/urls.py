38ed0d6bc0099b60c2fefa798170d922aaf3d237
morhaf-alsh <morhaf.alshoufi231@gmail.com>
2023-08-22 09:36:44 +0000

    m

--- car_parking/urls.py
+++ car_parking/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('parked/', include('parkManage.urls'), name='manage')
+    path('parked/', include('parkManage.urls'), name='park'),
+    path('store/', include('stroreManage.urls'), name='store'),
 ]
